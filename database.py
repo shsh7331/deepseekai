59b2c270e31e7e584e1f3b43b2c7c69c29c5b64c
shsh7331 <143574910+shsh7331@users.noreply.github.com>
2025-04-08 19:37:04 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
 DATABASE_URL = "postgresql://deepfinance_db_user:hWdcB41ynmNIfXELENWVhQJPolBpkdEb@dpg-cvqsje3e5dus73fu0lv0-a/deepfinance_db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
+@contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close(
+
+
